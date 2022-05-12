fb650fb0cf97c73f06f0f19afa4c6e7310c2f41b
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-11 23:49:00 -0300

    Additional controls & properties

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -1,20 +1,33 @@
 import "./mylabel.css";
 
 export interface MyLabelProps {
-    /**
-     * This is the message of the Label
-     */
+  /**
+    * This is the message of the Label
+  */
   label: string;
   /**
-   * This is the size of the label
-   */
+    * This is the size of the label
+  */
   size: "normal" | "h1" | "h2" | "h3";
+  /**
+    * State for capitalize string
+  */
+  allCaps?: boolean; 
+  /**
+    * Basic colors of the boton
+  */
+  color?: 'primary'|'secondary'|'tertiary';
 }
 
-export const MyLabel = ({ label = "Sin valor",  size = "normal"}: MyLabelProps) => {
+export const MyLabel = ({     
+    allCaps = false,
+    color   = 'primary',
+    label   = "Sin valor",  
+    size    = "normal"
+}: MyLabelProps) => {
         return(
-            <span className={`${size}`}>
-                    {label}
+            <span className={`${size} text-${ color } `}>
+                    { allCaps ? label.toUpperCase() : label }
             </span>
         )};
 export default MyLabel;
--- src/stories/components/MyLabel.stories.tsx
+++ src/stories/components/MyLabel.stories.tsx
@@ -4,16 +4,40 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 export default {
     title: 'Ui/MyLabel',
     component: MyLabel, 
+    argTypes: {
+        size: { control: 'select' },
+        color: { control: 'select' },
+    }
 } as ComponentMeta<typeof MyLabel>
  
 const Template: ComponentStory<typeof MyLabel> =(args) => <MyLabel {...args}/>
+
 export const Basic = Template.bind({});
 //Config por default en Basic storie
 Basic.args = {
     size: 'normal', 
-    label: 'Holaaa'
+    label: 'Texto de Prueba',
+    allCaps: false,
+    color: 'primary'
 }
 export const AllCaps = Template.bind({});
+AllCaps.args = {
+    size: 'normal',
+    allCaps: true, 
+}
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+    size: 'h2',
+    color: 'secondary' 
+}
+
+export const Tertiary = Template.bind({});
+Tertiary.args = {
+    size: 'h3',
+    color: 'tertiary' 
+}
+
 
 
 
